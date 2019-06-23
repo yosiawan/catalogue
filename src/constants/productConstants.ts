46fde012f8136c5bda70d55924aa75cec71b91d8
Yosia <yosiasetiawan@outlook.com>
2019-06-23 17:30:26 +0700

    added product constants

---
+++ src/constants/productConstants.ts
@@ -0,0 +1,11 @@
+// log
+export const GET_PRODUCTS = "GET_PRODUCTS"
+export const ADD_NEW_PRODUCT = "ADD_NEW_PRODUCT"
+export const UPDATE_PRODUCT = "UPDATE_PRODUCT"
+export const DELETE_PRODUCT = "DELETE_PRODUCT"
+
+// set state
+export const SET_PRODUCTS = "SET_PRODUCTS"
+export const SET_NEW_PRODUCT = "SET_NEW_PRODUCT"
+export const SET_UPDATED_PRODUCT = "SET_UPDATED_PRODUCT"
+export const SET_DELETE_PRODUCT = "SET_DELETE_PRODUCT"
