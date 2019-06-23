24b8f670710f595e891f4c13516448e16d4a3b6a
Yosia <yosiasetiawan@outlook.com>
2019-06-23 17:43:39 +0700

    updated index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createBrowserHistory } from "history";
+import { Router, Route } from "react-router";
+import { rootStore } from "./store";
 
-import App from './App';
+import Home from "./components/Home/Home";
+import Dashboard from "./components/Dashboard/Dashboard";
+import NavbarComponent from "./components/Navbar/Navbar";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
 
-import './index.css';
+const history = createBrowserHistory()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={rootStore}>
+    <Router history={history}>
+      <NavbarComponent>
+        <Route exact path="/" component={Home}/>
+        <Route path="/product/:id" component={ProductDetails}/>
+        <Route path="/dashboard" component={Dashboard}/>
+      </NavbarComponent>
+    </Router>
+  </Provider>,
+document.getElementById('root'));
